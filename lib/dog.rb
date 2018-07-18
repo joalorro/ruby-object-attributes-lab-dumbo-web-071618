f362c1e01fa9c21ef33dec75ad798fca6779227b
Josue V. Alorro <joalorro@gmail.com>
2018-07-18 17:12:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,3 +21,22 @@ class Dog
   end 
   
 end
+
+class Person 
+  def name 
+    @name 
+  end
+  
+  def name= new_name 
+    @name = new_name
+  end
+  
+  def job 
+    @job 
+  end
+  
+  def job= new_job
+    @job = new_job
+  end
+    
+end
